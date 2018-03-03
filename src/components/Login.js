754083c031073562f46501687b4ad9a32a29eeb2
Steve Roberts <mbudm.com@gmail.com>
2018-03-04 09:41:51 +1100

    prettier

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,59 +4,56 @@ import { connect } from 'react-redux';
 import { LOG_IN } from '../constants/actions';
 
 class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: ''
-        };
-    }
-    render() {
-        return (
-            <div>
-                <h2>Login</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <p>
-                        <label>Username</label>
-                        <input type="text" name="username"/>
-                    </p>
-                    <p>
-                        <label>Password</label>
-                        <input type="password" name="password"/>
-                    </p>
-                    <p>
-                        <input type="submit" value="Login" />
-                    </p>
-                </form>
-                <hr />
-                <ul>
-                    <li>
-                        <Link to="/forgot">Forgot Password</Link>
-                    </li>
-                </ul>
-            </div>
-        );
-    }
-    handleChange = (e) => {
-        this.setState({[e.target.name]: e.target.value});
-    }
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.onLogin({
-            username: this.state.username,
-            password: this.state.password
-        })
-    }
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <label>Username</label>
+            <input type="text" name="username" />
+          </p>
+          <p>
+            <label>Password</label>
+            <input type="password" name="password" />
+          </p>
+          <p>
+            <input type="submit" value="Login" />
+          </p>
+        </form>
+        <hr />
+        <ul>
+          <li>
+            <Link to="/forgot">Forgot Password</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onLogin({
+      username: this.state.username,
+      password: this.state.password
+    });
+  };
+}
 
-export default connect(
-    null,
-    dispatch => ({
-        onLogin(payload) {
-            dispatch({ 
-                type: LOG_IN, 
-                payload 
-            })
-        }
-    })
-)(Login);
+export default connect(null, dispatch => ({
+  onLogin(payload) {
+    dispatch({
+      type: LOG_IN,
+      payload
+    });
+  }
+}))(Login);
