e0b7fbd85f0cf1acb85b7d708beaa055e7b65d38
Steve Roberts <mbudm.com@gmail.com>
2018-03-08 21:31:15 +1100

    use remote auth
    hint need to sls s3Sync again to ensure the right UserPoolClientId is used

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,9 +1,11 @@
 const dev = {
-  getConfig: `http://localhost:3000/foto/config`
+  getConfig: (process.env.REACT_APP_USE_API_CONFIG ?
+    `${process.env.REACT_APP_FOTOPIA_API}foto/config`:
+    `http://localhost:3000/foto/config`)
 };
 
 const prod = {
-  getConfig: `${process.env.REACT_APP_FOTOPIA_API}/foto/config`
+  getConfig: `${process.env.REACT_APP_FOTOPIA_API}foto/config`
 };
 
 const config = process.env.NODE_ENV === 'production' ? prod : dev;
