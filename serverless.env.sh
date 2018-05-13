9b9e8fc34cc69b3eecde456391c45e9c7d465c51
Steve Roberts <steve.roberts@xero.com>
2018-05-13 18:59:45 +1000

    sh for generating env.yml

---
+++ serverless.env.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+FILE="serverless.env.yml"
+
+echo "dev:" > $FILE
+echo "  CUSTOM_DOMAIN: $CUSTOM_DOMAIN_DEV" >> $FILE
+echo "  BUCKET_NAME: $BUCKET_NAME_DEV" >> $FILE
+echo "  ACM_CERT_ARN: $ACM_CERT_ARN_DEV" >> $FILE
+echo "prod:" > $FILE
+echo "  CUSTOM_DOMAIN: $CUSTOM_DOMAIN_PROD" >> $FILE
+echo "  BUCKET_NAME: $BUCKET_NAME_PROD" >> $FILE
+echo "  ACM_CERT_ARN: $ACM_CERT_ARN_PROD" >> $FILE
+echo "HOSTED_ZONE_NAME: $HOSTED_ZONE_NAME" >> $FILE
