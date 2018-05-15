78d459f5e748e5a28f4bc922667bfd6ad472da94
Steve Roberts <mbudm.com@gmail.com>
2018-05-15 19:19:51 +1000

    fix overwriting of file

--- serverless.env.sh
+++ serverless.env.sh
@@ -6,7 +6,7 @@ echo "dev:" > $FILE
 echo "  CUSTOM_DOMAIN: $CUSTOM_DOMAIN_DEV" >> $FILE
 echo "  BUCKET_NAME: $BUCKET_NAME_DEV" >> $FILE
 echo "  ACM_CERT_ARN: $ACM_CERT_ARN_DEV" >> $FILE
-echo "prod:" > $FILE
+echo "prod:" >> $FILE
 echo "  CUSTOM_DOMAIN: $CUSTOM_DOMAIN_PROD" >> $FILE
 echo "  BUCKET_NAME: $BUCKET_NAME_PROD" >> $FILE
 echo "  ACM_CERT_ARN: $ACM_CERT_ARN_PROD" >> $FILE
