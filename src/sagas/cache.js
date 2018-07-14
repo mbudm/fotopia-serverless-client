40dedeb346fee53d3e4b73dbd06523310bb2eced
Steve Roberts <mbudm.com@gmail.com>
2018-07-14 07:19:34 +0200

    trigger search when sw detects loaded from cache
    Otherwise it misses the navigation saga that triggers this.. i think

---
+++ src/sagas/cache.js
@@ -0,0 +1,32 @@
+import { call, put, takeLatest } from 'redux-saga/effects';
+import {
+  SEARCH,
+  CACHED_LOAD
+} from '../constants/actions';
+import selectSearchResults from '../selectors/searchResults'
+import selectRoute from '../selectors/route';
+import {
+  HOME
+} from '../constants/routes';
+
+export default function* listenForCachedLoad() {
+  yield takeLatest(CACHED_LOAD, handleCachedLoad);
+}
+
+const getRefreshSearch = (state) => {
+  const results = selectSearchResults(state);
+  const route = selectRoute(state);
+  return results.length === 0 && route === HOME;
+}
+
+function* handleCachedLoad() {
+  // if on home page and results 0 then do a search
+  try {
+    const refreshSearch = yield select(getRefreshSearch);
+    if(refreshSearch){
+      yield put({ type: SEARCH });
+    }
+  } catch(e){
+    yield put({ type: CACHED_LOAD_FAILURE, payload: { error: e }});
+  }
+}
